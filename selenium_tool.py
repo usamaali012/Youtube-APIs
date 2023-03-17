acabb14747c0f67afb6b04fb33be8e7034da5784
Usama <usama@cognitivehealthintl.com>
2023-03-17 17:58:59 +0500

    Playlist Is Creating

--- selenium_tool.py
+++ selenium_tool.py
@@ -2,16 +2,12 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-# Create a new Chrome driver instance
 
 YOUTUBE_HOME_PAGE = 'https://www.youtube.com/'
-GOOGLE_SIGN_IN_PAGE = 'https://accounts.google.com/signin'
-# CHROME_PROFILE_PATH = r'C:\Users\CHI\AppData\Local\Google\Chrome\User Data\Profile 11'
 CHROME_PROFILE_PATH = r'C:\Users\CHI\AppData\Local\Google\Chrome\User Data'
 
 
@@ -27,19 +23,46 @@ def get_chrome_driver():
 
 
 def create_youtube_playlist():
+    t0 = time.time()
+
     driver = get_chrome_driver()
     driver.get(YOUTUBE_HOME_PAGE)
+    driver.maximize_window()
+
     wait = WebDriverWait(driver, 10)
 
     your_videos_css_selector = 'body > ytd-app:nth-child(4) > div:nth-child(7) > tp-yt-app-drawer:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > ytd-guide-renderer:nth-child(1) > div:nth-child(1) > ytd-guide-section-renderer:nth-child(1) > div:nth-child(2) > ytd-guide-collapsible-section-entry-renderer:nth-child(4) > div:nth-child(2) > ytd-guide-entry-renderer:nth-child(2) > a:nth-child(1) > tp-yt-paper-item:nth-child(1) > yt-formatted-string:nth-child(3)'
     your_videos_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, your_videos_css_selector)))
     your_videos_element.click()
 
-    create_path = "//div[contains(text(),'Create')]"
-    create_button_element = wait.until(EC.element_to_be_clickable((By.XPATH, create_path)))
-    create_button_element.click()
+    new_tab = driver.window_handles[-1]
+    driver.switch_to.window(new_tab)
 
-    time.sleep(5)
+    title_prefix = 'Testing1 Playlist'
 
+    for i in range(1, 1001):
+        create_path = r"create-icon"
+        create_button_element = wait.until(EC.element_to_be_clickable((By.ID, create_path)))
+        create_button_element.click()
 
-create_youtube_playlist()
+        new_playlist_path = "body > ytcp-app:nth-child(2) > ytcp-entity-page:nth-child(9) > div:nth-child(2) > ytcp-header:nth-child(3) > header:nth-child(1) > div:nth-child(4) > ytcp-text-menu:nth-child(8) > tp-yt-paper-dialog:nth-child(1) > tp-yt-paper-listbox:nth-child(2) > tp-yt-paper-item:nth-child(5) > ytcp-ve:nth-child(1) > div:nth-child(6) > div:nth-child(1) > yt-formatted-string:nth-child(2)"
+        new_playlist_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, new_playlist_path)))
+        new_playlist_element.click()
+
+        title_x_path = "//ytcp-social-suggestions-textbox[@id='title-textarea']//div[@id='textbox']"
+        title_element = wait.until(EC.element_to_be_clickable((By.XPATH, title_x_path)))
+        title_element.send_keys(f'{title_prefix} {i}')
+
+        save_button_path = "ytcp-button[id='create-button'] div[class='label style-scope ytcp-button']"
+        save_button_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, save_button_path)))
+        save_button_element.click()
+
+        if i == 1:
+            time.sleep(10)
+        else:
+            time.sleep(3)
+
+        print(f'Playlist {i} Created')
+
+    t1 = time.time()
+    print('It took', round((t1 - t0), 1), 'Seconds TO create 1000 Playlist')
